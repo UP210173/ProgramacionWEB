8a73b8f3887d03ae42406470b7b8bc34f7e07be1
Cristian Morales Perez <up210173@alumnos.upa.edu.mx>
2024-04-08 11:45:44 -0600

    Web

--- ASYNC-PROMISE/app.js
+++ ASYNC-PROMISE/app.js
@@ -1,20 +1,91 @@
-function getUsers(callback){
-    setTimeout(()=> {
+function getUsers (callback) {
+    setTimeout(() => {
         const users = [
-            {name: "Rogelio", years: 22},
-            {name: "Cristian", years: 20}
+            { name: "Cristian", years: 20 },
+            { name: "Andrea", years: 20 }
         ];
         callback(users);
-    },200)      
+    }, 2000);
 }
 
-function getInfo(name,callback){
+function getUsersWithPromise() {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const users = [
+                { name: "Cristian", years: 20 },
+                { name: "Andrea", years: 20 }
+            ];
+            resolve(users);
+        }, 2000);
+    });
+    return promise;
+}
+
+
+function getInfo (name, callback) {
     setTimeout(() => {
-        const saludo = "Que tranza " + name + " , ¿Como esta?";
+        let error = null
+        const saludo = "Hello " + name +", how are yoy?";
+        
+        if ( name === "Cristian") {
+            error = new Error ("He is not a real person")
+        }
 
+        callback(saludo, error);
+    }, 5000);
+}
+
+function getInfoWithPromise (name) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const saludo = "Hello" + name +", how are you?";
+            if ( name === "Cristian") {
+                reject( new Error ("He is not a real person") );
+            } else {
+                resolve(saludo);
+            }
+        }, 5000);
+    });
+
+}
+
+getUsers((users) => {
+    for (let i =0; i < users.length; i++) {
+        getInfo( users[i].name, (saludo, error) => {
+            if  (error !== null) {
+                console.log("There is ", error);
+            } else {
+                console.log(saludo);
+            }
+        });
+    }
+})
+
+getUsersWithPromise()
+    .then((users) => {
+        let newRespromises=[];
+        for (let i=0; i< users.length; i++) {
+            newRespromises.push(getInfoWithPromise(users[i].name))
+        }
+        return Promise.all(newRespromises);
     })
-}
-  
-function getUserWithPromise(){
-    const promise 
-}
+    .then((info) =>{
+        console.log(info);
+    })
+    .catch((error) =>{
+        console.log(error);
+    }); 
+    
+    async function main(){
+        let users = await getUsersWithPromise();
+        for (let i=0; i< users.length; i++) {
+            try {
+                let saludo = await getInfoWithPromise()
+                console.log(saludo);        
+            }catch (error) {
+                console.log(error);
+            }
+        }
+    }
+
+    main();
